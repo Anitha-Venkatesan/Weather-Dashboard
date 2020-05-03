82f03fa83b9e83c28d535a91e9c145e83a59d2de
Anitha Venkatesan <anithamca68@gmail.com>
2020-05-03 14:16:59 -0500

    Added styles for html element

--- script.js
+++ script.js
@@ -11,21 +11,15 @@ var apiKey= "89ac10db36e375ec24dd06e7440fc3a4";
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
-
         var temperature = response.main.temp
         temperature = (temperature - 273.15) * 1.80 + 32;
-        $(".name").text(response.name);
+        temperature=Number.parseFloat(temperature).toFixed(1); 
+        var todayDate=moment().format('l');
+        $(".name").text(response.name +" "+"("+todayDate+")");
         $(".temperature").text("Temperature: "+temperature+ " °F");
         $(".humidity").text("Humidity: "+response.main.humidity+ " %");
         $(".wind").text("Wind Speed: "+response.wind.speed+ " MPH");
-
-    
-
-
     
  });
-
- 
 
 });
